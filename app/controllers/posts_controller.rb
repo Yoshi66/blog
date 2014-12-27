c44b575a12f603f52f9bfecc79cddd602538e342
Yoshiki <yoshiki.mc@gmail.com>
2014-12-26 18:27:37 -0800

    stuck on select option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,19 @@ class PostsController < ApplicationController
 
   # GET /posts
   # GET /posts.json
+
+
+
+  #def options_for_elements
+    #@elements = Array(1..18).split(',')
+   # list = "H,He,Li,Be,B,C,N,O,F,Ne,Na,Mg,Al,Si,P,S,Cl,Ar,K,Ca,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Ga,Ge,As,Se,Br,Kr,Rb,Sr,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,In,Sn,Sb,Te,I,Xe,,Cs,Ba,Hf,Ta,W,Re,Os,Ir,Pt,Au,Hg,Tl,Pb,Bi,Po,At,Rn,Fr,Ra,Rf,Db,Sg,Bh,Hs,Mt,Ds,Rg,Cn,Uut,Fl,Uup,Lv,Uus,Uuo,La,Ce,Pr,Nd,Pm,Sm,Eu,Gd,Tb,Dy,Ho,Er,Tm,Yb,Lu,Ac,Th,Pa,U,Np,Pu,Am,Cm,Bk,Cf,Es,Fm,Md,No,Lr".split(',')
+    #total = Array.new
+    #list.each do |a|
+     # total << [a]
+      #logger.debug total
+    #end
+  #end
+
   def index
     @numbers = Array(1..18)
     @line1 = ['H', 'He']
@@ -11,6 +24,7 @@ class PostsController < ApplicationController
     @line3_1 = ['Na', 'Mg']
     @line3_2 = "Al,Si,P,S,Cl,Ar".split(',')
     @line4 = "K,Ca,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Ga,Ge,As,Se,Br,Kr".split(',')
+    logger.debug @line4
     @line5 = 'Rb,Sr,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,In,Sn,Sb,Te,I,Xe'.split(',')
     @line6 = 'Cs,Ba,*,Hf,Ta,W,Re,Os,Ir,Pt,Au,Hg,Tl,Pb,Bi,Po,At,Rn'.split(',')
     @line7 = 'Fr,Ra,**,Rf,Db,Sg,Bh,Hs,Mt,Ds,Rg,Cn,Uut,Fl,Uup,Lv,Uus,Uuo'.split(',')
